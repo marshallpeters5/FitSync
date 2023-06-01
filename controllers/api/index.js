d01bf7c17ecb1700154dfee3b3fac6063125c202
Marshall <marshall.peters01@gmail.com>
2023-06-01 18:29:31 -0400

    added a few changes to routes files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const exerciseRoutes = require('./exerciseRoutes');
+const userRoutes = require('./userRoutes.js');
+const exerciseRoutes = require('./exerciseRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/exercise', exerciseRoutes);
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,58 +2,68 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../../models");
 
+// Route for creating a new user
 router.post("/", async (req, res) => {
   try {
+    // Create a new user using the data from the request body
     const userData = await User.create(req.body);
-
+    // Save the user ID and set logged_in flag in the session
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-
+      // Respond with the created user data
       res.status(200).json(userData);
     });
   } catch (err) {
+    // If an error occurs, respond with the error
     res.status(400).json(err);
   }
 });
 
+// Route for user login
 router.post("/login", async (req, res) => {
   try {
+    // Find a user with the provided email in the request body
     const userData = await User.findOne({ where: { email: req.body.email } });
-
     if (!userData) {
+      // If no user is found, respond with an error message
       res
         .status(400)
         .json({ message: "Incorrect email or password, please try again" });
       return;
     }
-
+    // Check if the provided password matches the user's password
     const validPassword = await userData.checkPassword(req.body.password);
-
     if (!validPassword) {
+      // If the password is incorrect, respond with an error message
       res
         .status(400)
         .json({ message: "Incorrect email or password, please try again" });
       return;
     }
 
+    // Save the user ID and set logged_in flag in the session
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-
+      // Respond with the user data and a success message
       res.json({ user: userData, message: "You are now logged in!" });
     });
   } catch (err) {
+    // If an error occurs, respond with the error
     res.status(400).json(err);
   }
 });
 
+// Route for user logout
 router.post("/logout", (req, res) => {
   if (req.session.logged_in) {
+    // Destroy the session and respond with a success status
     req.session.destroy(() => {
       res.status(204).end();
     });
   } else {
+    // If no session is found, respond with a not found status
     res.status(404).end();
   }
 });
